f16328737e66a8fdfcfa2306c0abc653f0e9f3cf
ferraina <ferraina.matthias@gmail.com>
2019-11-28 23:17:36 +0100

    created new project

---
+++ stocktrading/src/views/Contact.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is the contact page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'contact',
+};
+</script>
