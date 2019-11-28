0cc9857f3e9e1d4c4671f2d56d40caf218d83805
ferraina <ferraina.matthias@gmail.com>
2019-11-29 00:45:44 +0100

    new tests added

--- stocktrading/tests/unit/example.spec.js
+++ stocktrading/tests/unit/example.spec.js
@@ -1,12 +1,28 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { shallowMount } from "@vue/test-utils";
+import HelloWorld from "@/components/HelloWorld.vue";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+describe("HelloWorld.vue", () => {
+    let wrapper;
+    const msg = "new message";
+    const items = ["Apple", "Computer", "Glass"];
+    beforeEach(() => {
+        wrapper = shallowMount(HelloWorld, {
+            propsData: { msg }
+        });
     });
-    expect(wrapper.text()).toMatch(msg);
-  });
+    it("renders props.msg when passed", () => {
+        expect(wrapper.text()).toMatch(msg);
+    });
+    it("should capitalize text", () => {
+        expect(wrapper.vm.capitalize("tits")).toMatch("TITS");
+    });
+    it("should not capitalize number", () => {
+        expect(wrapper.vm.capitalize(5)).toEqual(5);
+    });
+    it("count items sould return the number of items", () => {
+        expect(wrapper.vm.countItems(items)).toEqual(3);
+    });
+    it("count items sould throw an error", () => {
+        expect(wrapper.vm.countItems(3)).toEqual(-1);
+    });
 });
