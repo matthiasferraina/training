be12e891d89a02f6c0d3872f7c984deed2fb8af0
Matthias FERRAINA <mferraina.prestataire@si2m.fr>
2020-02-21 10:54:02 +0100

    utilisation de yargs

---
+++ node/modules/params.js
@@ -0,0 +1,4 @@
+const firstName = "Matthias";
+const lastName = "Ferraina";
+
+module.exports = {firstName, lastName}
---
+++ node/notes-app.js
@@ -0,0 +1,47 @@
+const chalk = require("chalk");
+const params = require("./modules/params.js");
+const yargs = require("yargs");
+
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder:{
+      title:{
+          describe:'note title',
+          demandOption:true,
+          type:"string"
+      }
+  },
+  handler: function(argv) {
+    console.log(chalk.green.inverse.bold("Adding a new note : ",argv.title));
+  }
+});
+
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function() {
+    console.log("Removing a note !");
+  }
+});
+
+// create list command
+yargs.command({
+  command: "list",
+  describe: "list the notes",
+  handler: function() {
+    console.log("Listing notes !");
+  }
+});
+
+// create read command
+yargs.command({
+  command: "read",
+  describe: "read the notes",
+  handler: function() {
+    console.log("Reading notes !");
+  }
+});
+yargs.parse();
