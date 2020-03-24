eb110942897c94672713d00e0ef8e33a2e22f1ed
ferraina <ferraina.matthias@gmail.com>
2020-03-24 15:36:23 +0100

    output current wether

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,10 +1,11 @@
 const request = require("request");
 const url =
-  "https://api.darksky.net/forecast/1cbff62b5ec2b8c45b134517f93dad8f/37.8267,-122.4233";
+    "https://api.darksky.net/forecast/1cbff62b5ec2b8c45b134517f93dad8f/37.8267,-122.4233?units=si&lang=fr";
 
-request({ url: url }, (error, reponse) => {
-  const data = JSON.parse(reponse.body);
-  const currentWeather = data.currently;
-  console.log(currentWeather);
-  console.log(currentWeather);
+request({ url: url, json: true }, (error, response) => {
+    const currentWeather = response.body.currently;
+
+    console.log(
+        `${response.body.daily.data[0].summary}. The temperature is currently of ${currentWeather.temperature} celsius. Chances of rain are :${currentWeather.precipProbability} %`
+    );
 });
