5dcdb20de87048a1344da1ceeedfdb7495ac19f1
ferraina <ferraina.matthias@gmail.com>
2020-03-26 20:16:10 +0100

    partials setup and 404 page

--- node/web-server/src/app.js
+++ node/web-server/src/app.js
@@ -1,16 +1,18 @@
 const path = require("path");
-
 const express = require("express");
 const hbs = require("hbs");
+
 const app = express();
 
 //Define paths for express configuration
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -32,12 +34,28 @@ app.get("/about", (req, res) => {
 app.get("/help", (req, res) => {
     res.render("help", {
         title: "Help for you",
+        creator: "Matthias FERRAINA",
         info: "Try to get a city weather !"
     });
 });
 
+app.get("/help/*", (req, res) => {
+    res.render('404', {
+        title: "404 not found",
+        errorMessage: 'Help article not found',
+        creator: "Matthias FERRAINA",
+    });
+});
 app.get("/weather", (req, res) => {
     res.send("weather page");
+});
+
+app.get("*", (req, res) => {
+    res.render('404', {
+        title: "404 not found",
+        errorMessage: 'Page not found',
+        creator: "Matthias FERRAINA",
+    });
 });
 
 app.listen(3000, () => {
